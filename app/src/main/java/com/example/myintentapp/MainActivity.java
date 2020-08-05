d3530fcc01f5af290f306fdc6105db0570f5fc4f
Abyravka90 <asepcahya.nugraha75@gmail.com>
2020-08-05 13:45:13 +0700

    adding two intent, intent with data and intent with dial activity

--- app/src/main/java/com/example/myintentapp/MainActivity.java
+++ app/src/main/java/com/example/myintentapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myintentapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,14 +17,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btnMoveActivity = findViewById(R.id.btn_move_activity);
         btnMoveActivity.setOnClickListener(this);
+
+        Button btnMoveActivityData = findViewById(R.id.btn_move_activity_data);
+        btnMoveActivityData.setOnClickListener(this);
+
+        Button btnDialPhone = findViewById(R.id.btn_dial_number);
+        btnDialPhone.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btn_move_activity:
+                case R.id.btn_move_activity:
                 Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
                 startActivity(moveIntent);
-            break;
+                break;
+
+                case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME,"Asep Cahya Nugraha");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE,30);
+                startActivity(moveWithDataIntent);
+                break;
+
+
+
+            case R.id.btn_dial_number:
+                String phoneNumber = "081210841382";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
+                startActivity(dialPhoneIntent);
+                break;
+
         }
     }
 }
